6db4ab1f9e44351975f942e0cdb2668a111e0715
DiegoCaldas24 <140341633+DiegoCaldas24@users.noreply.github.com>
2025-05-05 16:03:59 -0500

    Delete src/main/java/com/example/Ejemplo/interfaces directory
    delete directorys unusables

--- src/main/java/com/example/Ejemplo/interfaces/Muestras.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ejemplo.interfaces;
-
-import java.util.List;
-
-public interface Muestras<T> {
-    public List<T> list();
-}
--- src/main/java/com/example/Ejemplo/interfaces/Operaciones.java
+++
@@ -1,8 +0,0 @@
-package com.example.Ejemplo.interfaces;
-
-public interface Operaciones<T> {
-    public void add(T data);
-    public void update(T data);
-    public void delete(int id);
-    public T find(int id);
-}
