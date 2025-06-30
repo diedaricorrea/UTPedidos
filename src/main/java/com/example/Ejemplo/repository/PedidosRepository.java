e130bc263da294d690a41373ba10e131f1ea4231
DiegoCaldas24 <juandiegoarmandoc@gmail.com>
2025-06-29 20:46:30 -0500

    add pedidosRepository
    aplicacion del respositorio de pedidos

--- src/main/java/com/example/Ejemplo/repository/PedidosRepository.java
+++ src/main/java/com/example/Ejemplo/repository/PedidosRepository.java
@@ -1,4 +1,12 @@
 package com.example.Ejemplo.repository;
 
-public interface PedidosRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Ejemplo.models.Pedido;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PedidosRepository extends JpaRepository<Pedido,Integer> {
+    public List<Pedido> findPedidosNotEstatus(int estado);
 }
--- src/main/java/com/example/Ejemplo/services/PedidosService.java
+++ src/main/java/com/example/Ejemplo/services/PedidosService.java
@@ -1,4 +1,9 @@
 package com.example.Ejemplo.services;
 
+import com.example.Ejemplo.models.Pedido;
+
+import java.util.List;
+
 public interface PedidosService {
+    List<Pedido> findAllPedidos();
 }
